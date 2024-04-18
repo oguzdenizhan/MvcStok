e2288d26268c7f3f22366bba0f0ec6cba6a9a09a
Oğuzhan Denizhan <oguzhandenizhan25@gmail.com>
2024-04-18 16:17:11 +0300

    Lesson 18 Category  Add

--- MvcStok/Controllers/KategoriController.cs
+++ MvcStok/Controllers/KategoriController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcStok.Models.Entity;
 
 namespace MvcStok.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        MvcDbStockEntities db = new MvcDbStockEntities();
         public ActionResult Index()
         {
+            var degerler = db.TBLKATEGORILER.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
             return View();
         }
     }
---
+++ MvcStok/Controllers/MusterilerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+namespace MvcStok.Controllers
+{
+    public class MusterilerController : Controller
+    {
+        // GET: Musteriler
+        MvcDbStockEntities db = new MvcDbStockEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLMUSTERILER.ToList();
+            return View(degerler);
+        }
+    }
+}
